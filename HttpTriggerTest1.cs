f15be81d1e2cb80b4e5fbebdd0e4934419a34f94
John J Kauflin <johnkauflin@gmail.com>
2026-08-08 16:40:40 -0400

    test deploy 8

--- HttpTriggerTest1.cs
+++ HttpTriggerTest1.cs
@@ -18,7 +18,7 @@ public class HttpTriggerTest1
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        // test comment #7
+        // test comment #8
         return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
